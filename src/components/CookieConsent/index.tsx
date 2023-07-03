7ea58f30c3665a2f70831c773cfed22cc44f13e2
Ricardo M <rmoreiradev@gmail.com>
2023-07-04 00:22:53 +0100

    feat: added dark mode to cookie consent

--- src/components/CookieConsent/index.tsx
+++ src/components/CookieConsent/index.tsx
@@ -27,7 +27,7 @@ const CookieConsent: React.FC = () => {
     <AnimatePresence>
       {visible && (
         <motion.div
-          className="fixed -bottom-40 md:-bottom-20 left-0 w-full px-8 py-4 bg-white text-black shadow-black shadow-lg border-t flex flex-col md:flex-row items-center justify-between gap-4"
+          className="fixed -bottom-40 md:-bottom-20 left-0 w-full px-8 py-4 bg-white dark:bg-secondary-dark shadow-black shadow-lg border-t-gray-100 flex flex-col md:flex-row items-center justify-between gap-4"
           animate={{ y: [0, position] }}
           exit={{ y: [position, 0] }}
           transition={{ duration: 0.3, ease: 'easeOut' }}>
@@ -36,7 +36,7 @@ const CookieConsent: React.FC = () => {
               Usamos cookies essenciais para garantir que o Antirecurso funcione corretamente.{' '}
               <Link
                 href="/cookie-policy"
-                className="text-blue-700 hover:underline cursor-pointer"
+                className="text-blue-700 dark:text-blue-300 hover:underline cursor-pointer"
                 tabIndex={1}>
                 Sabe mais aqui.
               </Link>
