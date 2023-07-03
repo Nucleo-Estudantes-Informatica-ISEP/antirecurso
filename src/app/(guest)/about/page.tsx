e1e8dce9e5ccd124c89e13992ccb3431b57da58c
André Barros <1211299@isep.ipp.pt>
2023-07-03 11:59:08 +0100

    feat: added email support

--- src/app/(guest)/about/page.tsx
+++ src/app/(guest)/about/page.tsx
@@ -30,6 +30,22 @@ const About: React.FC = () => {
           </a>
         </p>
       </div>
+      <div className="h-0.5 w-5/6 bg-primary rounded opacity-70"></div>
+      <div className="flex items-center justify-between gap-x-2 w-3/4 md:w-1/2">
+        <h1 className="text-xl w-full font-semibold text-center mx-auto">
+          Encontraste algum <span className="text-primary">erro</span> ou queres enviar uma{' '}
+          <span className="text-primary">resolução</span> de um exame?{' '}
+          <span className="text-primary">Fala connosco!</span>
+        </h1>
+      </div>
+      <div className="space-y-3 w-5/6">
+        <p className="text-xl font-semibold uppercase">EMAIL</p>
+        <p className="text-primary font-semibold">
+          <a href="mailto:support.antirecurso@nei-isep.org" rel="noreferrer" target="_blank">
+            support.antirecurso@nei-isep.org
+          </a>
+        </p>
+      </div>
     </section>
   );
 };
---
+++ src/components/ContactUs/index.tsx
@@ -0,0 +1,5 @@
+const ContactUs = () => {
+  return <div className="flex flex-col md:flex-row gap-y-4 items-center "></div>;
+};
+
+export default ContactUs;
