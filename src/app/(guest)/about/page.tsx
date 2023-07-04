f31eb9269f4e1b6835b77a059daf66f465ed3a51
Tomás Lopes <1211289@isep.ipp.pt>
2023-07-04 12:36:46 +0100

    chore: added changelog link to about page

--- src/app/(guest)/about/page.tsx
+++ src/app/(guest)/about/page.tsx
@@ -1,33 +1,42 @@
+import Link from 'next/link';
+
 const About: React.FC = () => {
   return (
     <section className="flex flex-col items-center w-full space-y-10 text-center mt-8">
       <div className="space-y-3">
         <p className="text-xl font-semibold uppercase">Made by</p>
         <p className="font-semibold text-primary">
-          <a href="https://www.nei-isep.org/" rel="noreferrer" target="_blank">
+          <Link href="https://www.nei-isep.org/" rel="noreferrer" target="_blank">
             NEI-ISEP
-          </a>
+          </Link>
         </p>
       </div>
 
       <div className="space-y-3">
         <p className="text-xl font-semibold uppercase">Data Mining by</p>
         <p className="font-semibold text-primary">
-          <a href="https://github.com/tomasflopes" rel="noreferrer" target="_blank">
+          <Link href="https://github.com/tomasflopes" rel="noreferrer" target="_blank">
             Tomás Lopes
-          </a>
+          </Link>
         </p>
       </div>
 
       <div className="w-5/6 space-y-3">
         <p className="text-xl font-semibold uppercase">Git repository </p>
         <p className="w-full font-semibold text-primary">
-          <a
+          <Link
             rel="noreferrer"
             href="https://github.com/Nucleo-Estudantes-Informatica-ISEP/antirecurso"
             target="_blank">
             https://github.com/Nucleo-Estudantes-Informatica-ISEP/antirecurso
-          </a>
+          </Link>
+        </p>
+      </div>
+
+      <div className="w-5/6 space-y-3">
+        <p className="text-xl font-semibold uppercase">versions</p>
+        <p className="w-full font-semibold text-primary">
+          <Link href="/changelog">Check the new features</Link>
         </p>
       </div>
     </section>
