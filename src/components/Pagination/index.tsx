a0b7a192804b53e07e1c9ff019e9780d118769bc
Ricardo M <rmoreiradev@gmail.com>
2023-07-04 22:59:08 +0100

    feat: improved pagination

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -8,20 +8,46 @@ interface PaginationProps {
   setFetchUrl: Dispatch<SetStateAction<string | null>>;
 }
 
-const MAX_LINKS = 8;
-const MAX_MOBILE_LINKS = 4;
+const MAX_LINKS = 7;
+const MAX_MOBILE_LINKS = 5;
 
 const Pagination: React.FC<PaginationProps> = ({ metadata, setFetchUrl }) => {
   const { width } = document.documentElement.getBoundingClientRect();
-
   const maxLinks = width > 768 ? MAX_LINKS : MAX_MOBILE_LINKS;
+
+  const links = [];
+
+  if (metadata.links.length <= maxLinks) {
+    links.push(...metadata.links);
+  } else {
+    const threshold = Math.ceil(maxLinks / 2);
+    const { current_page, last_page } = metadata;
+
+    const lowerBound =
+      current_page < threshold // current page is close to the first page
+        ? 2
+        : current_page > last_page - threshold // current page is close to the last page
+        ? last_page - threshold - 1
+        : current_page - threshold / 2;
+
+    const upperBound =
+      current_page > last_page - threshold // current page is close to the last page
+        ? last_page - 1
+        : current_page < threshold // current page is close to the first page
+        ? threshold + 2
+        : current_page + threshold / 2;
+
+    links.push(metadata.links[0]); // prev btn
+    links.push(metadata.links[1]); // first page
+    for (let i = lowerBound; i <= upperBound; i++) links.push(metadata.links[i]);
+    links.push(metadata.links[last_page]); // last page
+    links.push(metadata.links[last_page + 1]); // next btn
+  }
 
   return (
     <nav className="w-full">
       <ul className="flex justify-center items-center py-6 gap-x-3">
-        {metadata.links.map((link) => {
-          if (!Number.isNaN(parseInt(link.label)) && parseInt(link.label) > maxLinks) return;
-
+        {links.map((link) => {
           return (
             <li key={link.label}>
               <button
