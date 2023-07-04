d4c8cf7a5ba1ac1ed7b6a6e9c3ff1ceabed02ae3
Ricardo M <rmoreiradev@gmail.com>
2023-07-04 18:32:22 +0100

    chore: improved profile avatar animation

--- src/components/UserAvatar/index.tsx
+++ src/components/UserAvatar/index.tsx
@@ -1,8 +1,6 @@
 'use client';
 
-import { useState } from 'react';
-
-import { motion } from 'framer-motion';
+import { AnimationProps, motion } from 'framer-motion';
 import Image from 'next/image';
 import Link from 'next/link';
 
@@ -11,13 +9,22 @@ interface UserAvatarProps {
 }
 
 const UserAvatar: React.FC<UserAvatarProps> = ({ avatar }) => {
-  const [isHovering, setIsHovering] = useState(false);
+  const animation: AnimationProps = {
+    variants: {
+      initial: { opacity: 0 },
+      hover: { opacity: 1 }
+    },
+    transition: {
+      duration: 0.2,
+      ease: 'easeOut'
+    }
+  };
 
   return (
-    <div
-      onMouseOver={() => setIsHovering(true)}
-      onMouseLeave={() => setIsHovering(false)}
-      className="flex flex-col items-center relative hover:cursor-pointer md:w-40 md:h-40 w-24 h-24 my-6">
+    <motion.div
+      initial="initial"
+      whileHover="hover"
+      className="flex flex-col items-center relative hover:cursor-pointer md:w-40 md:h-40 w-24 h-24 my-6 rounded-full">
       <Image
         placeholder="blur"
         blurDataURL="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjU2IiBoZWlnaHQ9IjI1NiIgdmlld0JveD0iMCAwIDI1NiAyNTYiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAw"
@@ -27,27 +34,22 @@ const UserAvatar: React.FC<UserAvatarProps> = ({ avatar }) => {
         alt="Avatar"
         className="w-full h-full rounded-full"
       />
-      {isHovering && (
-        <Link
-          href="https://en.gravatar.com/gravatars/new/computer"
-          target="_blank"
-          rel="noreferrer"
-          className="w-full h-full rounded-full absolute top-0 left-0 flex justify-center items-center text-white text-sm z-20">
-          <motion.div
-            animate={{
-              opacity: [0, 1]
-            }}
-            transition={{
-              duration: 0.3
-            }}
-            className="w-full h-full rounded-full absolute top-0 left-0 bg-primary bg-opacity-70"
-          />
-          <span className="z-10 text-white text-center text-sm leading-3 md:text-lg font-bold">
-            Alterar avatar
-          </span>
-        </Link>
-      )}
-    </div>
+      <Link
+        href="https://en.gravatar.com/gravatars/new/computer"
+        target="_blank"
+        rel="noreferrer"
+        className="w-full h-full rounded-full absolute top-0 left-0 flex justify-center items-center text-white text-sm z-20">
+        <motion.div
+          {...animation}
+          className="w-full h-full rounded-full absolute top-0 left-0 bg-primary bg-opacity-70"
+        />
+        <motion.span
+          {...animation}
+          className="z-10 text-white text-center text-sm leading-3 md:text-lg font-bold">
+          Alterar avatar
+        </motion.span>
+      </Link>
+    </motion.div>
   );
 };
 
