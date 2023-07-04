6402e14a24201e205f9bcc0c334405c41c604191
Tomás Lopes <1211289@isep.ipp.pt>
2023-07-04 14:02:14 +0100

    fix: fixed support link

--- src/app/(guest)/about/page.tsx
+++ src/app/(guest)/about/page.tsx
@@ -2,11 +2,15 @@ import Link from 'next/link';
 
 const About: React.FC = () => {
   return (
-    <section className="flex flex-col items-center w-full space-y-10 text-center mt-8">
+    <section className="flex flex-col items-center w-full space-y-10 text-center my-8">
       <div className="space-y-3">
         <p className="text-xl font-semibold uppercase">Made by</p>
         <p className="font-semibold text-primary">
-          <Link href="https://www.nei-isep.org/" rel="noreferrer" target="_blank">
+          <Link
+            className="hover:brightness-110"
+            href="https://www.nei-isep.org/"
+            rel="noreferrer"
+            target="_blank">
             NEI-ISEP
           </Link>
         </p>
@@ -15,7 +19,11 @@ const About: React.FC = () => {
       <div className="space-y-3">
         <p className="text-xl font-semibold uppercase">Data Mining by</p>
         <p className="font-semibold text-primary">
-          <Link href="https://github.com/tomasflopes" rel="noreferrer" target="_blank">
+          <Link
+            className="hover:brightness-110"
+            href="https://github.com/tomasflopes"
+            rel="noreferrer"
+            target="_blank">
             Tomás Lopes
           </Link>
         </p>
@@ -26,6 +34,7 @@ const About: React.FC = () => {
         <p className="w-full font-semibold text-primary">
           <Link
             rel="noreferrer"
+            className="hover:brightness-110"
             href="https://github.com/Nucleo-Estudantes-Informatica-ISEP/antirecurso"
             target="_blank">
             https://github.com/Nucleo-Estudantes-Informatica-ISEP/antirecurso
@@ -36,24 +45,30 @@ const About: React.FC = () => {
       <div className="w-5/6 space-y-3">
         <p className="text-xl font-semibold uppercase">versions</p>
         <p className="w-full font-semibold text-primary">
-          <Link href="/changelog">Check the new features</Link>
+          <Link className="hover:brightness-110" href="/changelog">
+            Check the new features
+          </Link>
         </p>
       </div>
+
       <div className="h-0.5 w-5/6 bg-primary rounded opacity-70"></div>
-      <div className="flex items-center justify-between gap-x-2 w-3/4 md:w-1/2">
+      <div className="gap-x-2 w-3/4 md:w-1/2 space-y-3">
         <h1 className="text-xl w-full font-semibold text-center mx-auto">
           Encontraste algum <span className="text-primary">erro</span> ou queres enviar uma{' '}
           <span className="text-primary">resolução</span> de um exame?{' '}
           <span className="text-primary">Fala connosco!</span>
         </h1>
-      </div>
-      <div className="space-y-3 w-5/6">
-        <p className="text-xl font-semibold uppercase">EMAIL</p>
-        <p className="text-primary font-semibold">
-          <Link href="mailto:support.antirecurso@nei-isep.org" rel="noreferrer" target="_blank">
-            support.antirecurso@nei-isep.org
-          </Link>
-        </p>
+        <div className="w-full">
+          <p className="text-primary font-semibold md:text-lg text-base">
+            <Link
+              className="hover:brightness-110"
+              href="mailto:support.antirecurso@nei-isep.org"
+              rel="noreferrer"
+              target="_blank">
+              support.antirecurso@nei-isep.org
+            </Link>
+          </p>
+        </div>
       </div>
     </section>
   );
