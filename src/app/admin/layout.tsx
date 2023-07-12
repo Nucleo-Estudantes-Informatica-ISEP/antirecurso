dfa460066d0f4a21213105401b09351aeb7ecc4e
André Barros <1211299@isep.ipp.pt>
2023-07-12 11:25:51 +0100

    fix: aside bar styling

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -8,7 +8,7 @@ const admin: React.FC<AdminLayoutProps> = ({ children }) => {
   return (
     <div className="h-full w-full flex items-center justify-center">
       <div className="flex w-full h-full">
-        <aside className="w-[15vw] bg-gray-100 dark:bg-secondary-dark py-4">
+        <aside className="fixed h-full w-[14vw] bg-gray-100 dark:bg-secondary-dark py-4">
           <h2 className="text-2xl font-bold text-center">Options</h2>
           <ul className="flex flex-col gap-y-4 ml-2 mt-4">
             <li className="text-lg">
@@ -38,7 +38,7 @@ const admin: React.FC<AdminLayoutProps> = ({ children }) => {
             </li>
           </ul>
         </aside>
-        <main className="w-full h-full flex">{children}</main>
+        <main className="ml-16 relative w-full h-full flex">{children}</main>
       </div>
     </div>
   );
