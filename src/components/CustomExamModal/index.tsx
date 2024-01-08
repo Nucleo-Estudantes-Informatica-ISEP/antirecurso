bb54aa68efad724c5eec0d2acffeca83bc8a111d
Tomás Russo <tomas.russo4@gmail.com>
2024-01-08 10:56:12 +0000

    styles: fix custom exam modal styles

--- src/components/CustomExamModal/index.tsx
+++ src/components/CustomExamModal/index.tsx
@@ -51,21 +51,21 @@ const CustomExamModal: React.FC<ModalProps> = ({ isVisible, setIsVisible, title,
 
   return (
     <div
-      className={`fixed left-0 top-0 h-screen w-screen bg-gray-500/60 p-20 z-20 items-center justify-center ${
+      className={`fixed left-0 top-0 h-screen w-full bg-gray-500/60 z-20 items-center justify-center ${
         isVisible ? 'fixed' : 'hidden'
       }`}>
-      <div className="fixed left-0 top-0 z-20 flex h-screen w-screen items-center justify-center">
+      <div className="fixed left-0 top-0 z-20 flex h-screen w-full items-center justify-center">
         <div
-          className={`flex w-3/5 flex-col rounded-lg p-16 bg-gray-200 dark:bg-gray-700 items-center gap-y-6 justify-around relative min-h-[520px]`}>
+          className={`flex w-full md:w-1/2 flex-col mx-6 rounded-lg p-14 lg:px-32 bg-gray-200 dark:bg-gray-700 items-center gap-y-6 justify-around relative min-h-[520px]`}>
           <button
             onClick={() => setIsVisible(false)}
             className="text-2xl font-black text-red-500 hover:text-red-600 z-20 absolute top-10 right-10">
             X
           </button>
-          <span className="w-full text-center text-3xl font-black">{title}</span>
-          <div className="max-h-[75vh] overflow-y-auto w-4/5">
-            <div className="flex flex-col items-center justify-between">
-              <h2 className="w-full font-bold mb-2 text-lg text-center md:text-left">
+          <span className="w-full text-center text-3xl font-black mb-6 px-2">{title}</span>
+          <div className="max-h-[75vh] overflow-y-auto w-full">
+            <div className="flex flex-col items-center justify-between mb-12">
+              <h2 className="w-full font-bold mb-6 text-lg text-center md:text-left">
                 Número de Questões
               </h2>
               <Slider min={1} max={50} value={numberOfQuestions} onChange={setNumberOfQuestions} />
@@ -76,10 +76,10 @@ const CustomExamModal: React.FC<ModalProps> = ({ isVisible, setIsVisible, title,
               </span>
             </div>
             <div className="flex flex-col items-center justify-between gap-x-8 w-full mt-6">
-              <h2 className="w-full font-bold mb-2 text-lg text-center md:text-left">
+              <h2 className="w-full font-bold mb-6 text-lg text-center md:text-left">
                 Penalização nas perguntas
               </h2>
-              <div className="w-full border-white border h-full md:h-10 flex flex-col md:flex-row items-center justify-center rounded-lg">
+              <div className="w-full border-gray-400 border h-full md:h-10 flex flex-col md:flex-row items-center justify-center rounded-lg">
                 {discounts.map((d, i) => (
                   <button
                     className={`${
@@ -88,7 +88,7 @@ const CustomExamModal: React.FC<ModalProps> = ({ isVisible, setIsVisible, title,
                         : i === discounts.length - 1
                         ? 'border-b-lg md:rounded-r-lg border-t md:border-l'
                         : 'border-t md:border-l'
-                    } border-white w-full h-8 md:h-full hover:bg-primary hover:text-white
+                    } md:border-x-gray-400 border-y-gray-400 md:border-y-0 w-full h-8 md:h-full hover:bg-primary hover:text-white
                     ${discount === d.value ? 'bg-primary text-white' : 'bg-transparent'}
                     `}
                     key={d.value}
@@ -103,9 +103,9 @@ const CustomExamModal: React.FC<ModalProps> = ({ isVisible, setIsVisible, title,
             </div>
           </div>
           <button
-            name="criar exame"
+            name="Criar Exame"
             onClick={handleSaveClick}
-            className="w-4/5 mt-4 p-2 text-xl dark:text-white rounded-md text-black bg-gray-300 dark:bg-primary-dark font-bold enabled:hover:bg-primary enabled:dark:hover:bg-primary hover:text-white"
+            className="w-full mt-8 p-2 text-xl bg-primary rounded-md text-white disabled:bg-gray-300 dark:disabled:bg-gray-600 disabled:text-gray-500 font-bold enabled:hover:brightness-90 enabled:hover:text-white"
             disabled={discount === null}>
             Criar exame
           </button>
