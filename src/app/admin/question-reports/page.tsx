d577b74abec147857f3f5510c25253ee042351e0
André L Reis <gato5andre26@gmail.com>
2024-01-14 18:28:21 +0000

    feat: select table rows feature

--- src/app/admin/question-reports/page.tsx
+++ src/app/admin/question-reports/page.tsx
@@ -1,14 +1,28 @@
 import ReportTable from '@/components/ReportTable';
+import fetchReports from '@/utils/FetchReports';
+import { BASE_URL } from '@/services/api';
 
 // @ts-expect-error Server Component
 const reports: React.FC = async () => {
 
+  const reports = await fetchReports(`${BASE_URL}/question-reports`);
 
   return (
     <div className="w-full h-full mt-4 flex flex-col items-center justify-center">
       <h2 className="text-4xl font-black">Reports</h2>
+      <div className="flex gap-x-2">
+        <button
+          className="bg-gray-400 hover:bg-gray-500 text-white px-4 py-2 rounded-md shadow-md"
+        // onClick={handleApply}
+        >
+          Resolver
+        </button>
+        <button className="bg-red-500 hover:bg-red-500 text-white px-4 py-2 rounded-md shadow-md">
+          Apagar
+        </button>
+      </div>
       <div className="flex flex-col w-3/4">
-        <ReportTable />
+        <ReportTable reports={reports} />
       </div>
     </div>
   );
--- src/components/ReportTable/index.tsx
+++ src/components/ReportTable/index.tsx
@@ -1,54 +1,86 @@
-import Report from '@/types/Report';
-import fetchReports from '@/utils/FetchReports';
-import { BASE_URL } from '@/services/api';
+"use client";
 
-// @ts-expect-error Server Component
-const ReportTable: React.FC = async () => {
-    const reports = await fetchReports(`${BASE_URL}/question-reports`);
+import Report from '@/types/Report';
+import { useState } from 'react';
+
+type TableProps = {
+    reports: Report[];
+};
+
+
+const ReportTable: React.FC<TableProps> = ({ reports }) => {
+
+    const [selectedRows, setSelectedRows] = useState<number[]>([]);
+
+    const toggleRow = (id: number) => {
+        setSelectedRows((prevSelectedRows) =>
+            prevSelectedRows.includes(id)
+                ? prevSelectedRows.filter((rowId) => rowId !== id)
+                : [...prevSelectedRows, id]
+        );
+    };
+
+    const handleSelectAll = () => {
+        const allRowIds = reports.map((row) => row.id);
+        setSelectedRows((prevSelectedRows) =>
+            prevSelectedRows.length === allRowIds.length ? [] : allRowIds
+        );
+    };
+
 
     return (
-        <table className="table-auto">
+        <table className="min-w-full border border-gray-300">
             {reports.length === 0 ? (
                 <thead>
                     <tr>
-                        <th className="px-4 py-2">Nenhum comentário encontrado</th>
+                        <th className="px-4 py-2">Nenhum report encontrado</th>
                     </tr>
                 </thead>
             ) : (
                 <>
                     <thead>
                         <tr>
+                            <th className="px-4 py-2">
+                                <input
+                                    type="checkbox"
+                                    checked={selectedRows.length === reports.length}
+                                    onChange={handleSelectAll}
+                                />
+                            </th>
                             <th className="px-4 py-2">ID</th>
                             <th className="px-4 py-2">Autor</th>
                             <th className="px-4 py-2">Descrição</th>
-                            <th className="px-4 py-2">Question ID</th>
+                            <th className="px-4 py-2">ID Questão</th>
                             <th className="px-4 py-2">Criado em</th>
-                            <th className="px-4 py-2">Solved</th>
-                            <th className="px-4 py-2">Selecionar</th>
+                            <th className="px-4 py-2">Resolvido</th>
                         </tr>
                     </thead>
                     <tbody>
                         {reports.map((report: Report) => (
-                            <tr key={report.id}>
+                            <tr
+                                key={report.id}
+                                className={selectedRows.includes(report.id) ? 'bg-blue-200' : ''}
+                            >
+                                <td className="border px-4 py-2">
+                                    <input
+                                        type="checkbox"
+                                        checked={selectedRows.includes(report.id)}
+                                        onChange={() => toggleRow(report.id)}
+                                    />
+                                </td>
                                 <td className="border px-4 py-2">{report.id}</td>
                                 <td className="border px-4 py-2">{report.user}</td>
                                 <td className="border px-4 py-2">{report.reason}</td>
                                 <td className="border px-4 py-2">{report.question.id}</td>
                                 <td className="border px-4 py-2">{report.created_at}</td>
                                 <td className="border px-4 py-2">{report.solved == 1 ? 'Sim' : 'Não'}</td>
-                                <td className="border px-4 py-2">
-                                    <input
-                                        type="checkbox"
-                                        name={`checkbox-${report.id}`}
-                                        id={`checkbox-${report.id}`}
-                                    />
-                                    <label htmlFor={`checkbox-${report.id}`}></label>
-                                </td>
+
                             </tr>
                         ))}
                     </tbody>
                 </>
             )}
+
         </table>
     );
 };
