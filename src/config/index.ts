d5c01f792311f1350b90f09b766bc65a378e96d2
Tomás Lopes <1211289@isep.ipp.pt>
2024-02-08 13:38:35 +0000

    chore: fixed linting issues

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,14 +1,13 @@
 const config = {
   cookies: {
-    token: `@AntiRecurso:token${process.env.NODE_ENV === "development" ? "_dev" : ""
-      }`,
+    token: `@AntiRecurso:token${process.env.NODE_ENV === 'development' ? '_dev' : ''}`
   },
   localStorage: {
-    consent: "@AntiRecurso:consent",
-    changelog: "@AntiRecurso:changelog",
+    consent: '@AntiRecurso:consent',
+    changelog: '@AntiRecurso:changelog'
   },
-  mandatoryAuthModes: ["new", "realistic", "wrong", "hard", "custom"],
-  version: "1.4.0",
+  mandatoryAuthModes: ['new', 'realistic', 'wrong', 'hard', 'custom'],
+  version: '1.4.0'
 };
 
 export default config;
