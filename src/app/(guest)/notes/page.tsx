f08006b9eb15c1232db617bbef8ec5a57b6fd035
Tomás Lopes <1211289@isep.ipp.pt>
2024-02-08 13:30:12 +0000

    fix: added missing loading text

--- src/app/(guest)/notes/page.tsx
+++ src/app/(guest)/notes/page.tsx
@@ -27,34 +27,42 @@ const Exams: React.FC = () => {
           <span className="text-primary">resumos</span>
         </p>
 
-        <section className="grid grid-cols-2 gap-x-4 px-6 md:grid-cols-4 gap-y-10 md:gap-x-10 my-8 md:px-16">
-          {subjects.map((subject) => (
-            <div
-              key={subject.id}
-              className={`relative w-full h-full md:h-48 p-6 flex flex-col space-y-6 items-center justify-center shadow dark:shadow-secondary-dark border border-gray-100 rounded text-center group ${
-                session
-                  ? 'hover:bg-primary transition ease-in-out'
-                  : 'pointer-events-none opacity-50'
-              }`}>
-              <Link
+        {subjects ? (
+          <section className="grid grid-cols-2 gap-x-4 px-6 md:grid-cols-4 gap-y-10 md:gap-x-10 my-8 md:px-16">
+            {subjects.map((subject) => (
+              <div
                 key={subject.id}
-                href={`/notes/${subject.id}`}
-                className="w-full h-full md:h-48 min-w-[160px] flex flex-col space-y-6 items-center justify-center text-center group transition ease-in-out">
-                <div className="w-full justify-center items-center overflow-auto">
-                  <p className="w-full text-xs md:text-xl font-bold capitalize line-clamp-6 group-hover:text-white">
-                    {subject.name}
-                  </p>
-                  <p className="text-xs mt-4 uppercase group-hover:text-white">({subject.slug})</p>
-                </div>
-              </Link>
-              {!session && (
-                <div className="absolute left-0 w-full p-1 text-xs font-bold text-white bg-red-500 md:text-base -top-2 md:-right-8 md:p-2">
-                  <p>Conta necessária 🔒</p>
-                </div>
-              )}
-            </div>
-          ))}
-        </section>
+                className={`relative w-full h-full md:h-48 p-6 flex flex-col space-y-6 items-center justify-center shadow dark:shadow-secondary-dark border border-gray-100 rounded text-center group ${
+                  session
+                    ? 'hover:bg-primary transition ease-in-out'
+                    : 'pointer-events-none opacity-50'
+                }`}>
+                <Link
+                  key={subject.id}
+                  href={`/notes/${subject.id}`}
+                  className="w-full h-full md:h-48 min-w-[160px] flex flex-col space-y-6 items-center justify-center text-center group transition ease-in-out">
+                  <div className="w-full justify-center items-center overflow-auto">
+                    <p className="w-full text-xs md:text-xl font-bold capitalize line-clamp-6 group-hover:text-white">
+                      {subject.name}
+                    </p>
+                    <p className="text-xs mt-4 uppercase group-hover:text-white">
+                      ({subject.slug})
+                    </p>
+                  </div>
+                </Link>
+                {!session && (
+                  <div className="absolute left-0 w-full p-1 text-xs font-bold text-white bg-red-500 md:text-base -top-2 md:-right-8 md:p-2">
+                    <p>Conta necessária 🔒</p>
+                  </div>
+                )}
+              </div>
+            ))}
+          </section>
+        ) : (
+          <div className="flex flex-col gap-y-8">
+            <p className="md:text-lg w-full">A carregar disciplinas...</p>
+          </div>
+        )}
       </section>
 
       <div className="flex flex-col gap-y-8">
