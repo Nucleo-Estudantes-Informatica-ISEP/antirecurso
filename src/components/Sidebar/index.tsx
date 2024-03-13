5f606da79cace95b9fb578ea86d7d69962964d40
Ricardo M <rmoreiradev@gmail.com>
2024-03-13 13:31:42 +0000

    feat: sidebar for mobile

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,13 +1,16 @@
 'use client';
 
-import { Flag, Home, LeftArrow, Message, Note, User } from '@/styles/Icons';
+import { Flag, Home, LeftArrow, Message, Note, RightArrow, User } from '@/styles/Icons';
 import Link from 'next/link';
+import { useState } from 'react';
 
 interface SidebarProps {
   children?: React.ReactNode;
 }
 
 const Sidebar: React.FC<SidebarProps> = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(true);
+
   const items = [
     { label: 'Dashboard', icon: <Home className="text-lg" />, link: '/admin' },
     { label: 'Example', icon: null, link: null },
@@ -20,14 +23,14 @@ const Sidebar: React.FC<SidebarProps> = () => {
 
   return (
     <>
-      <nav className="fixed w-64 -left-64 md:left-0 top-20 bottom-0 overflow-y-scroll z-30 shadow-md dark:bg-secondary-dark">
+      <nav
+        className={`${
+          !isOpen && 'max-md:hidden'
+        } fixed w-64 left-0 top-20 bottom-0 overflow-y-scroll z-20 shadow-md bg-white dark:bg-secondary-dark`}>
         <ul className="mb-12">
           <li className="mt-4 mb-2 font-bold dark:text-gray-300">
             <div className="px-4 flex flex-1 items-center justify-between">
               <span>Menu</span>
-              <button className="cursor-pointer">
-                <LeftArrow />
-              </button>
             </div>
           </li>
 
@@ -40,7 +43,8 @@ const Sidebar: React.FC<SidebarProps> = () => {
               {item.link ? (
                 <Link
                   href={item.link}
-                  className="flex flex-row items-center gap-4 w-full p-4 hover:bg-gray-200 dark:hover:text-white dark:hover:bg-gray-700 transition-colors">
+                  className="flex flex-row items-center gap-4 w-full p-4 hover:bg-gray-200 dark:hover:text-white dark:hover:bg-gray-700 transition-colors"
+                  onClick={() => setIsOpen(false)}>
                   {item.icon}
                   {item.label}
                 </Link>
@@ -54,7 +58,17 @@ const Sidebar: React.FC<SidebarProps> = () => {
         </ul>
       </nav>
 
-      <div className="fixed md:hidden top-20 bottom-0 left-0 right-0 bg-black/50 z-10"></div>
+      <div
+        className={`${
+          isOpen ? 'fixed' : 'hidden'
+        } md:hidden top-20 bottom-0 left-0 right-0 bg-black/50 z-10 transition-opacity`}
+        onClick={() => setIsOpen(false)}></div>
+
+      <button
+        className="absolute left-4 bottom-4 rounded-full p-4 shadow-md bg-white hover:bg-gray-200 dark:bg-primary-dark dark:hover:bg-gray-700 cursor-pointer md:hidden z-20 transition-colors"
+        onClick={() => setIsOpen((o) => !o)}>
+        {isOpen ? <LeftArrow /> : <RightArrow />}
+      </button>
     </>
   );
 };
