242dc0cfcba1ff9001b94d4ebf8883ba18fa2022
André Barros <103772884+andrefbarros@users.noreply.github.com>
2024-05-30 17:59:13 +0100

    chore: fixed build

--- src/components/LoadingSpinner/index.tsx
+++ src/components/LoadingSpinner/index.tsx
@@ -6,6 +6,7 @@ import { SVGAttributes } from 'react';
 import { LSpinner } from '@/styles/Icons';
 
 const LoadingSpinner: React.FC<SVGAttributes<SVGElement>> = ({ className }) => {
+  // @typescript-eslint/no-unused-vars
   const { theme } = useTheme();
 
   return <LSpinner className={`animate-spin ${className}`} />;
