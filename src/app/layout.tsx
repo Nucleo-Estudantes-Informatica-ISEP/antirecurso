9512070dcdc5762ae785906e5cee6500470750f8
André Barros <1211299@isep.ipp.pt>
2024-05-30 18:38:41 +0100

    fix: build errors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import ThemeProvider from '@/components/Theme/ThemeProvider';
 import Topbar from '@/components/Topbar';
 import { AuthContextProvider } from '@/contexts/AuthContext';
 import '@/styles/globals.css';
+import { Suspense } from 'react';
 
 export const metadata = {
   title: 'AntiRecurso',
@@ -16,11 +17,13 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body className="transition-colors dark:bg-primary-dark">
         <AuthContextProvider>
           <ThemeProvider>
-            <Topbar />
-            <main className="flex">
-              <div className="flex items-stretch w-full min-h-[calc(100vh-5rem)]">{children}</div>
-            </main>
-            <ChangelogPopUp />
+            <Suspense>
+              <Topbar />
+              <main className="flex">
+                <div className="flex items-stretch w-full min-h-[calc(100vh-5rem)]">{children}</div>
+              </main>
+              <ChangelogPopUp />
+            </Suspense>
             <CookieConsent />
           </ThemeProvider>
         </AuthContextProvider>
