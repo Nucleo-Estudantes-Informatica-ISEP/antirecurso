76c0480c0a4e18544fe22f01a5e6673e0c17e915
Tomás Lopes <1211289@isep.ipp.pt>
2024-07-04 03:46:22 +0100

    feat: added comment pagination

--- src/components/admin/CommentTable/index.tsx
+++ src/components/admin/CommentTable/index.tsx
@@ -1,49 +1,73 @@
+'use client';
+
+import LoadingSpinner from '@/components/utils/LoadingSpinner';
+import Pagination from '@/components/utils/Pagination';
+import useSession from '@/hooks/useSession';
+import { BASE_URL } from '@/services/api';
 import Comment from '@/types/Comment';
-import { fetchComments } from '@/utils/FetchComments';
+import { Paginate } from '@/types/Paginate';
+import { fetcher } from '@/utils/SWRFetcher';
+import { useState } from 'react';
+import useSWR from 'swr';
 
-const CommentTable: React.FC = async () => {
-  const comments = await fetchComments();
+const CommentTable: React.FC = () => {
+  const session = useSession();
+  const [fetchUrl, setFetchUrl] = useState<string | null>(`${BASE_URL}/comments`);
 
-  return (
-    <table className="table-auto">
-      {comments.length === 0 ? (
-        <thead>
-          <tr>
-            <th className="px-4 py-2">Nenhum comentário encontrado</th>
-          </tr>
-        </thead>
-      ) : (
-        <>
+  // conditional data fetching https://swr.vercel.app/docs/conditional-fetching
+  const { data: comments } = useSWR<Paginate<Comment>>(
+    session.token && fetchUrl ? [fetchUrl, session.token] : null,
+    ([url, token]) => fetcher(url, token as string),
+    { revalidateOnFocus: false }
+  );
+
+  return !comments ? (
+    <div className="flex items-center justify-center mt-16">
+      <LoadingSpinner className="text-6xl" />
+    </div>
+  ) : (
+    <>
+      <table className="table-auto">
+        {comments.data.length === 0 ? (
           <thead>
             <tr>
-              <th className="px-4 py-2">Autor</th>
-              <th className="px-4 py-2">Comentário</th>
-              <th className="px-4 py-2">Question ID</th>
-              <th className="px-4 py-2">Criado em</th>
-              <th className="px-4 py-2">Selecionar</th>
+              <th className="px-4 py-2">Nenhum comentário encontrado</th>
             </tr>
           </thead>
-          <tbody>
-            {comments.map((comment: Comment) => (
-              <tr key={comment.id}>
-                <td className="border px-4 py-2">{comment.user}</td>
-                <td className="border px-4 py-2">{comment.comment}</td>
-                <td className="border px-4 py-2">{comment.question_id}</td>
-                <td className="border px-4 py-2">{comment.created_at}</td>
-                <td className="border px-4 py-2">
-                  <input
-                    type="checkbox"
-                    name={`checkbox-${comment.id}`}
-                    id={`checkbox-${comment.id}`}
-                  />
-                  <label htmlFor={`checkbox-${comment.id}`}></label>
-                </td>
+        ) : (
+          <>
+            <thead>
+              <tr>
+                <th className="px-4 py-2">Autor</th>
+                <th className="px-4 py-2">Comentário</th>
+                <th className="px-4 py-2">Question ID</th>
+                <th className="px-4 py-2">Criado em</th>
+                <th className="px-4 py-2">Selecionar</th>
               </tr>
-            ))}
-          </tbody>
-        </>
-      )}
-    </table>
+            </thead>
+            <tbody>
+              {comments.data.map((comment: Comment) => (
+                <tr key={comment.id}>
+                  <td className="border px-4 py-2">{comment.user}</td>
+                  <td className="border px-4 py-2">{comment.comment}</td>
+                  <td className="border px-4 py-2">{comment.question_id}</td>
+                  <td className="border px-4 py-2">{comment.created_at}</td>
+                  <td className="border px-4 py-2">
+                    <input
+                      type="checkbox"
+                      name={`checkbox-${comment.id}`}
+                      id={`checkbox-${comment.id}`}
+                    />
+                    <label htmlFor={`checkbox-${comment.id}`}></label>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </>
+        )}
+      </table>
+      <Pagination metadata={comments.meta} setFetchUrl={setFetchUrl} />
+    </>
   );
 };
 
--- src/utils/FetchComments.ts
+++ src/utils/FetchComments.ts
@@ -1,9 +1,10 @@
 import { BASE_URL } from '@/services/api';
 import Comment from '@/types/Comment';
+import { Paginate } from '@/types/Paginate';
 import { cookies } from 'next/headers';
 import config from 'src/config';
 
-export async function fetchComments(): Promise<Comment[]> {
+export async function fetchComments(): Promise<Paginate<Comment>> {
   const t = cookies().get(config.cookies.token);
   const token = t?.value;
 
@@ -17,5 +18,5 @@ export async function fetchComments(): Promise<Comment[]> {
 
   if (res.status !== 200) throw new Error('Could not fetch comments');
 
-  return ((await res.json()) as { data: Comment[] }).data;
+  return await res.json();
 }
